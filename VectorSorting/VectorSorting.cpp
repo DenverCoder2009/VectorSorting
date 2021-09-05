112cfc968ba1e16bf953f1329785a21068dec94f
Jayd Cheshire <jcheshire36@gmail.com>
2021-09-04 19:28:05 -0700

    deleted useless stuff

--- VectorSorting/VectorSorting.cpp
+++ VectorSorting/VectorSorting.cpp
@@ -44,7 +44,7 @@ struct Bid {
 void displayBid(Bid bid) {
     cout << bid.bidId << ": " << bid.title << " | " << bid.amount << " | "
             << bid.fund << endl;
-    return;faaaaack you mother fucker son of a c
+    return;
 }
 
 /**
